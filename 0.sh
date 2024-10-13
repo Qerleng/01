c956c56a4450b09f99df74061dbdf2efdeb127d1
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-13 20:39:34 +0700

    Delete 0.sh

--- 0.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/DustinWin/clash_singbox-tools/releases/download/mihomo/mihomo-alpha-linux-amd64.tar.gz -O - | tar -zxf - -C ./tools/
-mv -f ./tools/CrashCore ./tools/mihomo
-wget https://github.com/DustinWin/clash_singbox-tools/releases/download/sing-box/sing-box-release-linux-amd64v3.tar.gz -O - | tar -zxf - -C ./tools/
-mv -f ./tools/CrashCore ./tools/sing-box
-curl -L -O https://github.com/zhihans/geodata/raw/master/tools/v2dat
-mkdir -p tools
-mv -f ./v2dat ./tools/
