44596b469b0e73a22bce1e0608b2a148dbed04a0
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-13 23:50:38 +0700

    Delete data directory

--- data/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p tools
-wget https://github.com/DustinWin/clash_singbox-tools/releases/download/mihomo/mihomo-alpha-linux-amd64.tar.gz -O - | tar -zxf - -C ./tools/
-mv -f ./tools/CrashCore ./tools/mihomo
-wget https://github.com/DustinWin/clash_singbox-tools/releases/download/sing-box/sing-box-release-linux-amd64v3.tar.gz -O - | tar -zxf - -C ./tools/
-mv -f ./tools/CrashCore ./tools/sing-box
