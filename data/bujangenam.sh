e682b53f2b20697eb646275e6c318fb6365f39e9
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-14 00:12:28 +0700

    Update bujangenam.sh

--- data/bujangenam.sh
+++ data/bujangenam.sh
@@ -66,5 +66,7 @@ done
 
 for file in test/*; do
     filename=$(basename "$file")
-    (cd test && mihomo convert-ruleset domain yaml $filename ${filename%.*}.mrs) &
+    (cd test && mihomo convert-ruleset domain yaml $filename ${filename%.*}.mrs && mv "${filename%.*}.mrs" Ads/) &
 done
+
+rm rf test/*
