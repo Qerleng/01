d923440bcb75e7969360496081aed068bb321390
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-14 14:56:42 +0700

    Update bujangenam.sh

--- data/bujangenam.sh
+++ data/bujangenam.sh
@@ -25,7 +25,7 @@ for file in *.abp; do
     srs_file="${filename%.*}.srs"
     json_file="${filename%.*}.json"
 
-    echo "abp ==>> yaml ~ Domain ==>> mrs"
+    # abp ==>> yaml ~ Domain ==>> mrs
     echo "payload:" > $txt_file && cat $file >> $txt_file
     sed -i 's/||\(.*\)\^/- "+.\1["]/' $txt_file
     sed -i 's/0.0.0.0 \(.*\)/- "\1["]/' $txt_file
@@ -33,13 +33,13 @@ for file in *.abp; do
     sed -i 's/^! /# /' $txt_file
     sed -i -e '/^#/d' -e '/^$/d' $txt_file
     sed -i -e '/^!/d' -e '/^$/d' $txt_file
-    echo "$txt_file\n"
+    echo "$txt_file"
     teks=$(basename "$txt_file")
     category=$(echo "$teks")
     output_file="${category%.*}.txt"
     (cd Ads && mihomo convert-ruleset domain yaml $output_file ${output_file%.*}.mrs && mv -if "$filename" ${filename%.*}.txt) 
 
-    echo "abp ==>> yaml ~ Classical"
+    # abp ==>> yaml ~ Classical
     echo "payload:" > $yaml_file && cat $file >> $yaml_file
     sed -i 's/||\(.*\)\^/  - DOMAIN-SUFFIX,\1/' $yaml_file
     sed -i 's/0.0.0.0 \(.*\)/  - DOMAIN-SUFFIX,\1/' $yaml_file
@@ -49,7 +49,7 @@ for file in *.abp; do
     sed -i -e '/^!/d' -e '/^$/d' $yaml_file
     echo "$yaml_file"
 
-    echo "yaml ==>> json srs"
+    # yaml ==>> json srs
     jq -R 'select(test("^  - DOMAIN-SUFFIX")) | split(",")[1]' $yaml_file | jq -s '{ "version": 1, "rules": [{ "domain_suffix": . }] }' > $json_file
     sing-box rule-set compile $json_file
     echo "$json_file"
