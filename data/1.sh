78065e909d01a436d38e5dda86a8063dc8b6becf
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-16 02:41:59 +0700

    Update 1.sh

--- data/1.sh
+++ data/1.sh
@@ -2,6 +2,6 @@ curl -LO https://github.com/Qerleng/01/raw/main/tools/sing-box
 
 chmod +x ./sing-box
 
-./sing-box geoip [command] --help | awk '{print}' | sort > categories
+./sing-box geoip -f --help | awk '{print}' | sort > categories
 
 rm -f sing-box
