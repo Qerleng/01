ac13803d4d51e637cc58e7733aa49bba83033378
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-16 02:27:14 +0700

    Create 1.sh

---
+++ data/1.sh
@@ -0,0 +1,6 @@
+for tool in tools/*; do
+    filename=$(basename "$tool")
+    command -v $filename &> /dev/null || { cp ./$filename /usr/local/bin/ && chmod +x /usr/local/bin/$filename; }
+done
+
+sing-box geoip -h --help > categories
