1952d6184b8eacbddd94a4d37d7237ce97678b64
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-16 03:04:22 +0700

    Update 1.sh

--- data/1.sh
+++ data/1.sh
@@ -1,10 +1,10 @@
 curl -LO https://github.com/Qerleng/01/raw/main/tools/sing-box
-curl -Lo geip.db "https://github.com/rfxcll/v2ray-rules-dat/releases/latest/download/GeoIP.db"
+curl -Lo geoip.db "https://github.com/rfxcll/v2ray-rules-dat/releases/latest/download/GeoIP.db"
 
 chmod +x ./sing-box
-chmod +x ./geip.db
+chmod +x ./geoip.db
 
 ./sing-box geoip --help > categories
-./sing-box geoip list -f .geip.db > geoip_categories
+./sing-box geoip list -f > geoip_categories
 
-rm -f sing-box geip.db
+rm -f sing-box geoip.db
