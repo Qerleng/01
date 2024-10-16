c73495428902ab0b7c5995c9f9e755c0a792ae46
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-16 14:43:43 +0700

    Update bujangenam.sh

--- data/bujangenam.sh
+++ data/bujangenam.sh
@@ -48,8 +48,8 @@ for file in *.abp; do
     sed -i -e '/^!/d' -e '/^$/d' $yaml_file
 
     # yaml ==>> json srs
-  #  jq -R 'select(test("^  - DOMAIN-SUFFIX")) | split(",")[1]' $yaml_file | jq -s '{ "version": 1, "rules": [{ "domain_suffix": . }] }' > $json_file
-    sing-box rule-set convert $file
+    jq -R 'select(test("^  - DOMAIN-SUFFIX")) | split(",")[1]' $yaml_file | jq -s '{ "version": 1, "rules": [{ "domain_suffix": . }] }' > $json_file
+    sing-box rule-set convert $json_file
     sing-box rule-set compile $json_file
 
     echo "$txt_file"
