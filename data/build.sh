258038d4cd88bf696a1043c42608140e48a54b07
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-16 14:41:59 +0700

    Update build.sh

--- data/build.sh
+++ data/build.sh
@@ -15,7 +15,7 @@ done
 
 
 geoipAddresses=("fastly" "doh" "malicious" "cloudfront" "id" "facebook" "google" "netflix" "telegram" "twitter")
-geositeDomains=("category-ads-all" "category-porn" "oisd-nsfw" "rule-doh" "rule-gaming" "rule-indo" "rule-playstore" "rule-sosmed" "rule-streaming" "rule-umum" "rule-ipcheck" "rule-speedtest" "videoconference" "rule-malicious" "urltest" "openai" "ecommerce-id" "bank-id")
+geositeDomains=("category-ads-all" "category-porn" "oisd-nsfw" "rule-doh" "rule-gaming" "rule-indo" "rule-playstore" "rule-sosmed" "rule-streaming" "rule-umum" "rule-ipcheck" "rule-speedtest" "videoconference" "rule-malicious" "urltest" "openai" "ecommerce-id" "whatsapp" "bank-id")
 
 
 for item in "${geoipAddresses[@]}"; do
