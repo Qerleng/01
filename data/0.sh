25752540edb1260d1913f10dfc61a77dd1c9e7eb
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-17 01:38:29 +0700

    Update 0.sh

--- data/0.sh
+++ data/0.sh
@@ -16,12 +16,12 @@ for line in mains:
     domain = line.split("$")[0].strip()
   if any(prefix in domain for prefix in ("autodesk", "github", "tiktok", "pinterest", "pinimg", "twitter", "linkedin", "telegram", "facebook", "line", "instagram", "whatsapp")):
     continue
-  elif domain.startswith((".", "*", "/", "[", "(")):
+  elif any(prefix in domain for prefix in ("[", "*")):
     domains.append("DOMAIN-REGEX," + domain)
   else:
     domains.append("DOMAIN," + domain)
 
-payload = [f'"{line}"' for line in domains]
+payload = [f'{line}' for line in domains]
 
 data = {
   "payload": payload
