84dcdd23d4feb21617e68fa4fa44291008c00ba7
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-17 02:19:08 +0700

    Update 0.sh

--- data/0.sh
+++ data/0.sh
@@ -1,5 +1,5 @@
 echo "Downloading oisd full source list..."
-curl -sSf -o oisd-full.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_53.txt
+curl -sSf -o oisd-full.txt https://small.oisd.nl
 python3 - <<EOF
 import re
 import yaml
