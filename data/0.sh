3378673a0df888e225d40eb8532916f6bb81c725
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-17 13:28:39 +0700

    Update 0.sh

--- data/0.sh
+++ data/0.sh
@@ -14,7 +14,7 @@ mains = payload_lines + payload_lines2
 for line in mains:
   if line:
     domain = line.split("$")[0].strip()
-  if any(prefix in domain for prefix in ("#", "!", "@@", "||", "\\", "autodesk", "github", "tiktok", "pinterest", "pinimg", "twitter", "linkedin", "telegram", "facebook", "line", "instagram", "whatsapp")):
+  if any(prefix in domain for prefix in ("#", "!", "@@", "||", "autodesk", "github", "tiktok", "pinterest", "pinimg", "twitter", "linkedin", "telegram", "facebook", "line", "instagram", "whatsapp")):
     continue
   elif any(prefix in domain for prefix in ("[", "/", "*", "^")):
     domains.append("DOMAIN-REGEX," + domain)
