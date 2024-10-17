05b1f9473ed17c5f25e800f2be4ac3197dc96edf
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-17 13:38:44 +0700

    Create 01.sh

---
+++ data/01.sh
@@ -0,0 +1,32 @@
+for file in tools/*; do
+    filename=$(basename "$file")
+    command -v $filename &> /dev/null || { cp ./tools/$filename /usr/local/bin/ && chmod +x /usr/local/bin/$filename; }
+done
+
+
+for file in *.yaml; do
+    filename=$(basename "$file")
+    txt_file="${filename%.*}.txt"
+    yaml_file="${filename%.*}.yaml"
+    mrs_file="${filename%.*}.mrs"
+    category=$(echo "$txt_file")
+    output_file="${category%.*}.txt"
+    echo $txt_file && cat $file >> $txt_file
+    sed -i 's/  - DOMAIN-SUFFIX,\(.*\)/- "+.\1"/' $txt_file
+    sed -i 's/  - DOMAIN,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/  - DOMAIN-KEYWORD,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/  - DST-PORT,\(.*\)//' $txt_file
+    sed -i 's/  - DOMAIN-REGEX,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/\(.*\)/\1/' $txt_file
+    sed -i 's/^! /# /' $txt_file
+    sed -i -e '/^#/d' -e '/^$/d' $txt_file
+    sed -i -e '/^!/d' -e '/^$/d' $txt_file
+    (mihomo convert-ruleset domain yaml $output_file ${output_file%.*}.mrs) 
+
+    mkdir -p trash
+    mv "${file%.*}.txt" trash/
+    mv "${file%.*}.mrs" trash/
+    mv "${file%.*}.yaml" trash/
+    mv "${file%.*}.abp" trash/
+
+done
