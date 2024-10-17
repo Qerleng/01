767ea258e0c3dab142a6690da329bc18ff067b0e
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-17 13:26:15 +0700

    Update 0.sh

--- data/0.sh
+++ data/0.sh
@@ -1,10 +1,10 @@
 echo "Downloading oisd full source list..."
-curl -sSf -o oisd-full.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_53.txt
+curl -sSf -o AWAvenue.abp https://adguardteam.github.io/HostlistsRegistry/assets/filter_53.txt
 python3 - <<EOF
 import re
 import yaml
 
-with open("oisd-full.txt", "r") as file:
+with open("AWAvenue.abp", "r") as file:
   input_text = file.read()
 
 domains = []
@@ -16,7 +16,7 @@ for line in mains:
     domain = line.split("$")[0].strip()
   if any(prefix in domain for prefix in ("#", "!", "@@", "||", "\\", "//", "autodesk", "github", "tiktok", "pinterest", "pinimg", "twitter", "linkedin", "telegram", "facebook", "line", "instagram", "whatsapp")):
     continue
-  elif any(prefix in domain for prefix in ("[", "/", "*", "(", "^")):
+  elif any(prefix in domain for prefix in ("[", "/", "*", "^")):
     domains.append("DOMAIN-REGEX," + domain)
   else:
     domains.append("DOMAIN," + domain)
@@ -31,7 +31,7 @@ yaml_output = yaml.dump(data, sort_keys=False, default_flow_style=False)
 
 formatted_yaml = re.sub(r"(\s+-) '(.+)'", r"\1 \2", yaml_output)
 
-with open("rule_basicads.yaml", "w") as file:
+with open("AWAvenue.txt", "w") as file:
   file.write(formatted_yaml)
 EOF
 # mv -if oisd-full.txt 
