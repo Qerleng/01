45561c3074d423556f96b15aaf69c0f0b62c88c3
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-17 13:46:52 +0700

    Update 01.sh

--- data/01.sh
+++ data/01.sh
@@ -12,11 +12,11 @@ for file in *.yaml; do
     category=$(echo "$txt_file")
     output_file="${category%.*}.txt"
     echo $txt_file && cat $file >> $txt_file
-    sed -i 's/  - DOMAIN-SUFFIX,\(.*\)/- "+.\1"/' $txt_file
-    sed -i 's/  - DOMAIN,\(.*\)/- "\1"/' $txt_file
-    sed -i 's/  - DOMAIN-KEYWORD,\(.*\)/- "\1"/' $txt_file
-    sed -i 's/  - DST-PORT,\(.*\)//' $txt_file
-    sed -i 's/  - DOMAIN-REGEX,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/- DOMAIN-SUFFIX,\(.*\)/- "+.\1"/' $txt_file
+    sed -i 's/- DOMAIN,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/- DOMAIN-KEYWORD,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/- DST-PORT,\(.*\)//' $txt_file
+    sed -i 's/- DOMAIN-REGEX,\(.*\)/- "\1"/' $txt_file
     sed -i 's/\(.*\)/\1/' $txt_file
     sed -i 's/^! /# /' $txt_file
     sed -i -e '/^#/d' -e '/^$/d' $txt_file
