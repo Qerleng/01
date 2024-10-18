98713cf80703140cd309e532c193605f7bb506b0
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-18 22:39:25 +0700

    Update bujangenam.sh

--- data/bujangenam.sh
+++ data/bujangenam.sh
@@ -47,7 +47,7 @@ for file in *.abp; do
 
     # yaml ==>> json srs
     jq -R 'select(test("^  - DOMAIN-SUFFIX")) | split(",")[1]' $yaml_file | jq -s '{ "version": 1, "rules": [{ "domain_suffix": . }] }' > $json_file
-    sing-box rule-set convert "${file%.*}.abp" "${file%.*}.srs"
+    sing-box rule-set convert -t adguard -o "${file%.*}.abp" "${file%.*}.srs"
 
     echo "$txt_file"
     echo "$yaml_file"
