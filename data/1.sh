afd8c777e23079a2e1be06b6b0aa602da4cd8119
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-20 20:44:47 +0700

    Update 1.sh

--- data/1.sh
+++ data/1.sh
@@ -1,18 +1,25 @@
-curl -LO https://github.com/Qerleng/01/raw/main/tools/sing-box
-curl -LO https://github.com/Qerleng/01/raw/main/tools/mihomo
-curl -LO https://github.com/Qerleng/01/raw/main/tools/v2dat
+for file in *.yaml; do
+    filename=$(basename "$file")
+    txt_file="${filename%.*}.txt"
+    yaml_file="${filename%.*}.yaml"
+    mrs_file="${filename%.*}.mrs"
+    category=$(echo "$txt_file")
+    output_file="${category%.*}.txt"
+    echo $txt_file && cat $file >> $txt_file
+    sed -i 's/  - DOMAIN-SUFFIX,\(.*\)/- "+.\1"/' $txt_file
+    sed -i 's/  - DOMAIN,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/  - DOMAIN-KEYWORD,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/  - DST-PORT,\(.*\)//' $txt_file
+    sed -i 's/  - DOMAIN-REGEX,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/\(.*\)/\1/' $txt_file
+    sed -i 's/^! /# /' $txt_file
+    sed -i -e '/^#/d' -e '/^$/d' $txt_file
+    sed -i -e '/^!/d' -e '/^$/d' $txt_file
+    (mihomo convert-ruleset domain yaml $output_file ${output_file%.*}.mrs) 
 
 
+    rm "${file%.*}.txt" 
+    mv "${file%.*}.mrs" rule_provider/
+#    mv "${file%.*}.yaml" trash/
 
-chmod +x ./sing-box
-chmod +x ./mihomo
-chmod +x ./v2dat
-
-mkdir -p help
-
-./sing-box rule-set --help > help/categories
-./mihomo convert --help > help/categories1
-./v2dat unpack --help > help/categories2
-
-
-rm -f sing-box mihomo v2dat
+done
