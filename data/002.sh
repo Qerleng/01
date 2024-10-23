0bca2299fb7554cbb2c548f6f45509575e28a5d7
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-23 19:52:47 +0700

    Create 002.sh

---
+++ data/002.sh
@@ -0,0 +1,51 @@
+for file in tools/*; do
+    filename=$(basename "$file")
+    command -v $filename &> /dev/null || { cp ./tools/$filename /usr/local/bin/ && chmod +x /usr/local/bin/$filename; }
+done
+
+
+for file in *.yaml; do
+    filename=$(basename "$file")
+    txt_file="${filename%.*}.txt"
+    json_file="${filename%.*}.json"
+    yaml_file="${filename%.*}.yaml"
+    mrs_file="${filename%.*}.mrs"
+    category=$(echo "$txt_file")
+    echo $txt_file && cat $file >> $txt_file
+    sed -i 's/- DOMAIN-SUFFIX,\(.*\)/- "+.\1"/' $txt_file
+    sed -i 's/- DOMAIN,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/- DOMAIN-KEYWORD,\(.*\)/- "\1"/' $txt_file
+    sed -i 's/- DST-PORT,\(.*\)//' $txt_file
+    sed -i 's/- DOMAIN-REGEX,\(.*\)/- "\1"/' $txt_file
+    (mihomo convert-ruleset domain yaml $category ${category%.*}.mrs)
+    echo $yaml_file && cat $file >> $yaml_file
+    sed -i 's/-\(.*\)/  -\1/' $yaml_file
+    mv -if "${file%.*}.mrs" rule_provider/
+    mv -if "${file%.*}.abp" rule_provider/
+    mv -if "${file%.*}.txt" rule_provider/
+
+    jq -nR '{
+        version: 1,
+        rules: [
+            reduce (inputs | sub("^ *- *"; "") | select(length > 0) | split(",")) as $item ({};
+                if $item[0] == "DOMAIN-SUFFIX" then
+                    .domain_suffix += [$item[1]]
+                elif $item[0] == "DOMAIN-KEYWORD" then
+                    .domain_keyword += [$item[1]]
+                elif $item[0] == "DOMAIN-REGEX" then
+                    .domain_regex += [$item[1]]
+                elif $item[0] == "DOMAIN" then
+                    .domain += [$item[1]]
+                else
+                    .
+                end
+            )
+        ]
+    }' < $yaml_file > $json_file && \
+    sing-box rule-set format $json_file -w
+    sing-box rule-set compile $json_file
+    mv -if "${file%.*}.json" rule_provider/
+    mv -if "${file%.*}.srs" rule_provider/
+    mv -if "${file%.*}.yaml" rule_provider/
+    
+done
