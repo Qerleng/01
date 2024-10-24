b2c5ccf9a6b6bd3e43ecfc688f562c704e365b3c
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-24 18:17:49 +0700

    Create 98.sh

---
+++ data/98.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+
+curl -Lo geoip.db "https://github.com/rfxcll/v2ray-rules-dat/releases/latest/download/GeoIP.db"
+curl -Lo geosite.db "https://github.com/rfxcll/v2ray-rules-dat/releases/latest/download/GeoSite.db"
+curl -Lo geoip.dat "https://github.com/rfxcll/v2ray-rules-dat/releases/latest/download/GeoIP.dat"
+curl -Lo geosite.dat "https://github.com/rfxcll/v2ray-rules-dat/releases/latest/download/GeoSite.dat"
+
+for tool in tools/*; do
+    filename=$(basename "$tool")
+    command -v $filename &> /dev/null || { cp ./$filename /usr/local/bin/ && chmod +x /usr/local/bin/$filename; }
+done
+
+sing-box geoip list -f geoip.db | awk '{print $1}' | sort > geoip_categories.list
+while IFS= read -r category; do
+    v2dat unpack geoip -o "original/geoip" -f "$category" "geoip.dat" &
+done < geoip_categories.list
+
+rm -rf geoip.db geosite.db geoip.dat geosite.dat
