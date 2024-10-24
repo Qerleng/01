7849ff120295785fe010b0592fe1e59a6d24305b
Qerleng <140212474+Qerleng@users.noreply.github.com>
2024-10-24 23:28:06 +0700

    Update 88.sh

--- data/88.sh
+++ data/88.sh
@@ -2,7 +2,7 @@
 geoipAddresses=("firewall" "geoid" "private" "malicious" "id" "google" "netflix" "telegram" "twitter")
 
 
-for file in rule_provider/${geoipAddresses[@]}.yaml; do
+for file in rule_provider/${geoipAddresses[@]}; do
     filename=$(basename "$file")
     txt_file="${filename%.*}.txt"
     yaml_file="${filename%.*}.yaml"
@@ -12,7 +12,7 @@ for file in rule_provider/${geoipAddresses[@]}.yaml; do
     json_file="${filename%.*}.json"
 
     rm -rf $mrs_file
-    rm -rf $file
+    rm -rf $yaml_file
     rm -rf $srs_file
     rm -rf $json_file
 
